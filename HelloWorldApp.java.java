d492c2d98dfa5d61b610f4e804c1daf770ff4a98
Nattapon <TopNattaponKanjum@gmail.com>
2017-07-25 10:59:52 +0700

    เริ่มต้น Branch "HelloWorldApp10Time" ให้แสดง HelloWorld! 1 ถึง 10 ออกจากโปรแกรม

--- HelloWorldApp.java.java
+++ HelloWorldApp.java.java
@@ -6,5 +6,9 @@ class HelloWorldApp {
     public static void main(String[] args) {
         System.out.println("Hello World!"); // Display the string.
 // นักกินผัก
+	int i =1;
+	while(i < 11){
+	    System.out.println("Hello World!" + i);
+	}
     }
 }
